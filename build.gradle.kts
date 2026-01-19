40eb81d3f8cde73334240af80bd6910aec87e6f3
Zibete <matiasabelperalta@gmail.com>
2026-01-19 10:26:31 -0300

    fix(build): align Hilt with KSP for AGP 9 during edit-profile refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,23 @@
 // Top-level build file
 plugins {
     // 1. Android Gradle Plugin
-    id("com.android.application") version "8.13.2" apply false
-    id("com.android.library") version "8.13.2" apply false
+    id("com.android.application") version "9.0.0" apply false
+    id("com.android.library") version "9.0.0" apply false
 
     // 2. Kotlin
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
 
     // 3. Google Services
     id("com.google.gms.google-services") version "4.4.1" apply false
 
     // 4. Compose Compiler
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 
     // 5. HILT
-    id("com.google.dagger.hilt.android") version "2.52" apply false
-
+    id("com.google.dagger.hilt.android") version "2.58" apply false
 
     // 6. KSP
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
 }
 
 tasks.register("clean", Delete::class) {
