8e7b509d00c0b10631399ccb09e49735b1e4277c
Zibete <matiasabelperalta@gmail.com>
2026-01-27 09:25:50 -0300

    domain: add feedback and credentials update use cases

--- app/src/main/java/com/zibete/proyecto1/domain/profile/SendFeedbackUseCase.kt
+++ app/src/main/java/com/zibete/proyecto1/domain/profile/SendFeedbackUseCase.kt
@@ -1,2 +1,32 @@
 package com.zibete.proyecto1.domain.profile
 
+import com.zibete.proyecto1.core.device.DeviceInfoProvider
+import com.zibete.proyecto1.core.utils.ZibeResult
+import com.zibete.proyecto1.core.utils.getOrThrow
+import com.zibete.proyecto1.core.utils.zibeCatching
+import com.zibete.proyecto1.data.UserRepositoryActions
+import jakarta.inject.Inject
+
+interface SendFeedbackUseCase {
+    suspend fun execute(
+        feedback: String,
+        screen: String
+    ): ZibeResult<Unit>
+}
+
+class DefaultSendFeedbackUseCase @Inject constructor(
+    private val userRepositoryActions: UserRepositoryActions,
+    private val deviceInfoProvider: DeviceInfoProvider
+) : SendFeedbackUseCase {
+    override suspend fun execute(
+        feedback: String,
+        screen: String
+    ): ZibeResult<Unit> = zibeCatching {
+        userRepositoryActions.sendFeedback(
+            feedback = feedback,
+            screen = screen,
+            model = deviceInfoProvider.getModel(),
+            appVersion = deviceInfoProvider.getAppVersion()
+        ).getOrThrow()
+    }
+}
