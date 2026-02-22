9db2a339798d3c4aa7b87c49709d90927f7563a0
Zibete <matiasabelperalta@gmail.com>
2026-02-22 16:06:08 -0300

    chore(security): remove App Check debug logs and forced token request

--- app/src/main/java/com/zibete/proyecto1/core/utils/ZibeApp.kt
+++ app/src/main/java/com/zibete/proyecto1/core/utils/ZibeApp.kt
@@ -2,9 +2,7 @@ package com.zibete.proyecto1.core.utils
 
 import android.app.Application
 import android.content.Context
-import android.util.Log
 import com.google.firebase.appcheck.FirebaseAppCheck
-import com.zibete.proyecto1.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -15,10 +13,7 @@ class ZibeApp : Application() {
         val appCheck = FirebaseAppCheck.getInstance()
         val providerFactory = AppCheckProviderFactoryProvider.get()
         appCheck.installAppCheckProviderFactory(providerFactory)
-        Log.d("ZibeApp", "AppCheck initialized (${if (BuildConfig.DEBUG) "debug" else "release"})")
         appCheck.getAppCheckToken(false)
-            .addOnSuccessListener { Log.d("ZibeApp", "AppCheck token OK") }
-            .addOnFailureListener { Log.w("ZibeApp", "AppCheck token FAIL", it) }
 
         ScreenUtils.init(this)
     }
