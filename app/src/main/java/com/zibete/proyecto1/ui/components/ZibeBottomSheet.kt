d7d4a7cb6f3fadb9dcc7b0131997e7f062767348
Zibete <matiasabelperalta@gmail.com>
2026-07-13 21:18:15 -0300

    feat(ui): add zibe motion and interaction feedback

--- app/src/main/java/com/zibete/proyecto1/ui/components/ZibeBottomSheet.kt
+++ app/src/main/java/com/zibete/proyecto1/ui/components/ZibeBottomSheet.kt
@@ -39,6 +39,7 @@ import com.zibete.proyecto1.R
 import com.zibete.proyecto1.ui.theme.LocalZibeExtendedColors
 import com.zibete.proyecto1.ui.theme.LocalZibeTextStyles
 import com.zibete.proyecto1.ui.theme.ZibeTheme
+import com.zibete.proyecto1.ui.motion.ZibeHapticFeedback
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -150,7 +151,8 @@ fun SheetActions(
                 .weight(1f)
                 .fillMaxHeight(),
             enabled = confirmEnabled && !isConfirmLoading,
-            isLoading = isConfirmLoading
+            isLoading = isConfirmLoading,
+            hapticFeedback = ZibeHapticFeedback.Confirm
         )
     }
 }
@@ -205,7 +207,6 @@ fun ZibeBottomSheetPreviewField() {
         )
     }
 }
-
 
 
 
